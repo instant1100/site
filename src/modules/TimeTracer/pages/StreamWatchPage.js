d11ab6bba7b53bbcf47e56a9de4b462b10e18837
Зубков Валерий Павлович <v.zubkov@bssys.com>
2020-07-02 09:15:19 +0400

    fix

--- src/modules/TimeTracer/pages/StreamWatchPage.js
+++ src/modules/TimeTracer/pages/StreamWatchPage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import useDB from '@/core/AppDB/hooks/useDB';
-import { STREAM_ENTITY } from '$TimeTracer/entity/entityMap';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
 
-const useStyles = makeStyles((theme) => ({
+import { STREAM_ENTITY } from '$TimeTracer/entity/entityMap';
+
+
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
     minHeight: 250,
